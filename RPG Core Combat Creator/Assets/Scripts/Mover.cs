82788703ff217b99e0e7d38c35b81338bdbcc6eb
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2023-05-06 17:13:18 -0700

    Introduction To Raycasting (5_BM_RPG)
    Draw a ray from camera to point in scene
    Also implemented InputSystem and revised lesson to new input system
    Changed scene name to Sandbox

--- RPG Core Combat Creator/Assets/Scripts/Mover.cs
+++ RPG Core Combat Creator/Assets/Scripts/Mover.cs
@@ -2,18 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class Mover : MonoBehaviour
 {
     [SerializeField] private Transform target;
 
+    private Ray lastRay;
+    private PlayerControls playerControls;
+
     private NavMeshAgent navMeshAgent;
 
     private void Awake() {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        playerControls = new PlayerControls();
     }
 
-    private void Update() {
-        navMeshAgent.destination = target.position;
+    private void OnEnable() {
+        playerControls.Enable();
+        playerControls.Player.Move.performed += Move;
+    }
+
+    private void OnDisable() {
+        playerControls.Disable();
+    }
+
+    private void Move(InputAction.CallbackContext ctx) {
+        lastRay = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100, Color.red, 3f);
+        // navMeshAgent.destination = target.position;
     }
 }
