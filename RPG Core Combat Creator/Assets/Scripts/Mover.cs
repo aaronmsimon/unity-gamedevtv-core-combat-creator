d75f437d30e47f536a0591796bfee6800bfb0d51
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2023-05-07 14:00:37 -0700

    Match Animation To Movement (9_BM_RPG)
    Set speed for animation blend
    Use script to get agent's velocity, convert to local space, then pass the z value to the animator
    Also set angular speed and overall acceleration to tune so turning looks natural, too

--- RPG Core Combat Creator/Assets/Scripts/Mover.cs
+++ RPG Core Combat Creator/Assets/Scripts/Mover.cs
@@ -8,10 +8,12 @@ public class Mover : MonoBehaviour
 {
     private PlayerControls playerControls;
     private NavMeshAgent navMeshAgent;
+    private Animator animator;
 
     private void Awake() {
         playerControls = new PlayerControls();
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable() {
@@ -23,6 +25,10 @@ public class Mover : MonoBehaviour
         playerControls.Disable();
     }
 
+    private void Update() {
+        UpdateAnimator();
+    }
+
     private void Move(InputAction.CallbackContext ctx) {
         Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
         RaycastHit hitInfo;
@@ -30,5 +36,13 @@ public class Mover : MonoBehaviour
         if (Physics.Raycast(ray, out hitInfo)) {
             navMeshAgent.destination = hitInfo.point;
         }
+    }
+
+    private void UpdateAnimator() {
+        Vector3 worldVelocity = navMeshAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(worldVelocity);
+        float speed = localVelocity.z;
+
+        animator.SetFloat("forwardSpeed", speed);
     }
 }
