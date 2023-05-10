afbe4b5757178266d2df3a5487c3d469d20f55a2
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2023-05-09 19:55:20 -0700

    Attempt at challenge 6_BC_RPG

--- RPG Core Combat Creator/Assets/Scripts/Combat/Fighter.cs
+++ RPG Core Combat Creator/Assets/Scripts/Combat/Fighter.cs
@@ -1,11 +1,31 @@
 using UnityEngine;
+using RPG.Movement;
 
 namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour
     {
-        public void Attack() {
-            Debug.Log("made an attack!");
+        [SerializeField] private float weaponRange;
+
+        private Transform target;
+
+        private Mover mover;
+
+        private void Awake() {
+            mover = GetComponent<Mover>();
+        }
+
+        private void Update() {
+            if (target != null) {
+                mover.MoveTo(target.position);
+                if (Mathf.Abs((target.position - transform.position).magnitude) < weaponRange) {
+                    mover.Stop();
+                }
+            }
+        }
+
+        public void Attack(CombatTarget combatTarget) {
+            target = combatTarget.transform;
         }
     }
 }
