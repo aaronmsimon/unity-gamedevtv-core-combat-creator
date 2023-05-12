3f48c3a6522b0917c1d07e9481c6e97fd27ab389
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2023-05-11 19:49:18 -0700

    Trigger Death Animation (17_BC_RPG)
    Create Die function in Health class to trigger death animation and only make possible to die once
    Also set capsule collider position based on values

--- RPG Core Combat Creator/Assets/Scripts/Combat/Health.cs
+++ RPG Core Combat Creator/Assets/Scripts/Combat/Health.cs
@@ -4,11 +4,34 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
+
         [SerializeField] private float health = 100f;
+
+        private bool isDead;
+
+        private Animator animator;
+
+        private void Awake() {
+            animator = GetComponent<Animator>();
+        }
+
+        private void Start() {
+            isDead = false;
+        }
 
         public void TakeDamage(float damage) {
             health = Mathf.Max(health - damage, 0);
-            Debug.Log(health);
+            
+            if (health == 0) {
+                Die();
+            }
+        }
+
+        private void Die() {
+            if (!isDead) {
+                animator.SetTrigger("die");
+                isDead = true;
+            }
         }
     }
 }
